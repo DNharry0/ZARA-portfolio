fcd635d7172e6e117d1b8e3d4620be3cb4287723
harry <harrykcfa@gmail.com>
2022-07-19 18:27:27 +0900

    CSS: Responsive
    메인 포토 반응형 적용

--- src/component/MainPhoto.js
+++ src/component/MainPhoto.js
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 const MainPhoto = () => {
   return (
     <div className="mainphoto-section">
-      <Link to="1" duration={10}>
+      <Link to="1">
         <img
           className="mainphoto"
           src="https://static.zara.net/photos///2022/V/M/1/p/0000/003/538/2/w/942/0000003538_15_2_1.jpg?ts=1655198679625"
